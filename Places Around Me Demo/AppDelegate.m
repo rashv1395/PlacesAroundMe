36271b5dbc2fa1b8fe44802798d72149ce9360ad
Student-001 <student-001@felix-itss-macbook-pro-3.local>
2016-12-21 12:38:45 +0530

    this is 1st commit by master

--- Places Around Me Demo/AppDelegate.m
+++ Places Around Me Demo/AppDelegate.m
@@ -19,7 +19,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
    
-    
+    //rahul
     
     [Fabric with:@[[Crashlytics class]]];
 
